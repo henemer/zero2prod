7ecf2e33a67161bf3053e418005f902b4a7a7a3f
Emerson Henning <emerson@henning.com.br>
2023-04-19 11:05:36 -0300

    Add trace and log

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // panic if we can't read configuration
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    // LogTracer::init().expect("Failed to set logger");
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed do connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,27 @@
 //! tests/health_check.rs
 
 use sqlx::{Connection, Executor, PgConnection, PgPool};
+// use tracing::subscriber;
+use once_cell::sync::Lazy;
 use std::net::TcpListener;
 use uuid::Uuid;
+
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub address: String,
@@ -13,6 +30,8 @@ pub struct TestApp {
 
 // Launch our application in the backgroud
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     // We retrieve the port assigned to us by the OS
