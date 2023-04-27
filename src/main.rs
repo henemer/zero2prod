35f0542f1cf99c16ab1b4d7e26ce943723ab3ead
Emerson Henning <emerson@henning.com.br>
2023-04-27 13:56:50 -0300

    Add docker configuration

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 use zero2prod::configuration::get_configuration;
@@ -13,10 +15,11 @@ async fn main() -> Result<(), std::io::Error> {
     // LogTracer::init().expect("Failed to set logger");
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed do connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,10 @@
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
+// use actix_web::middleware::Logger;
+// use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::{health_check, subscribe};
 
@@ -15,7 +17,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
