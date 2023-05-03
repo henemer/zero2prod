4e4ce6845d12575e806bce3dd97232badf03fd3b
Emerson Henning <emerson@henning.com.br>
2023-05-03 16:46:01 -0300

    Add database to config app

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -56,7 +56,7 @@ async fn spawn_app() -> TestApp {
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
     let mut connection =
-        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+        PgConnection::connect_with(&config.without_db())
             .await
             .expect("Failed do connect to Postgres");
 
@@ -66,7 +66,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed do create database");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed do connect to Postgres");
 
